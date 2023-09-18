eba37e557476bb95b8c4c00ac4c90603d94bcc43
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-18 08:25:01 -0300

    asa

--- appdonar/src/App.js
+++ appdonar/src/App.js
@@ -4,11 +4,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Donar from './assets/donar';
 import Datos from './assets/datos';
-
+import Navbar from '../assets/NabVar';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/donar" element={<Donar />}></Route>
--- appdonar/src/components/Home.jsx
+++ appdonar/src/components/Home.jsx
@@ -1,13 +1,11 @@
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from "react";
-import NabVar from '../assets/NabVar';
 import EspacioC from '../assets/EspacioC';
 
 export default function Home (){
     return (
         <>
-        <NabVar></NabVar>
         <EspacioC></EspacioC>
         </>
     )
